ff706dc062f7bfec651d5c024e3d5ba57619b01d
Mostafa Aziz Zuher <thecool4a@gmail.com>
2024-02-16 16:14:41 +0100

    Finished unfair HungryBear solution

--- Homework/Homework 3/HungryBear.c
+++ Homework/Homework 3/HungryBear.c
@@ -20,66 +20,87 @@ Is your solution fair (w.r.t. honeybees)? Explain when presenting homework.
 #define DEFAULT_NUMBER_OF_BEES 10
 #define MAX_NUMBER_OF_BEES 30
 
+
 sem_t potIsNotFull;
 sem_t potIsFull;
 sem_t potAccess;
-
+sem_t yourTurn;
 int h = 10;
 int pot = 0;
+int globalseed;
 
 
-void* Bear(void*);
-void* Bee(void*);
+void* Bear(void* arg);
+void* Bee(void* arg);
 
 void* Bear(void* arg)
 {
-    //wait untill pot is full
-    sem_wait(&potIsFull);
-    sem_wait(&potAccess);
-    printf("Bear is eating from the honey pot");
-    usleep(100000);
-    pot = 0;
-    printf("Bear is sleeping");
-    sem_post(&potAccess);
-    sem_post(&potIsNotFull);
-   
-    //eat all honey in pot
+    while(1) {
+        sem_wait(&potIsFull);
+        sem_wait(&potAccess);
+        printf("bear eats from the honey pot\n");
+        usleep(100000);
+        pot = 0;
+        printf("pot is empty\n");
+        printf("bear sleeps\n");
+        sem_post(&potAccess);
+        sem_post(&potIsNotFull);
+    }
 }
 
-void* Bee(void* arg)
-{
-    //fill pot
-    //if pot full
-    //wake bear
+void* Bee(void* arg) {
+    long rank = (long) arg;
 
+    unsigned int mySeed = globalseed ^ rank;
+    int myTravelTime = 0;
+    while(1) {
+        sem_wait(&yourTurn);
+        sem_wait(&potAccess);
+
+        printf("bee %ld fills pot with honey\n", rank);
+        pot++;
+        printf("pot has %d units of honey\n", pot);
+        if(pot == h) {
+            sem_post(&potAccess);
+            printf("bee %ld wakes up bear\n", rank);
+            sem_post(&potIsFull);
+            sem_wait(&potIsNotFull);
+        }
+        else {
+            sem_post(&potAccess);
+        }
+        printf("bee %ld fetches honey\n", rank);
+        sem_post(&yourTurn);
+        myTravelTime = rand_r(&mySeed) % 200000 + 20;
+        usleep(myTravelTime);
+    }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     long i;
     int numberOfBees = (argc > 1) ? atoi(argv[1]) : DEFAULT_NUMBER_OF_BEES;
     if (numberOfBees > 30) {
         numberOfBees = MAX_NUMBER_OF_BEES;
     }
+    globalseed = time(0);
     pthread_t bee[numberOfBees];
     pthread_t bear;
-    sem_init(potAccess, 0, 1);
-    sem_init(potIsNotFull, 0, 1);
-    sem_init(potIsFull, 0, 0);
+    sem_init(&potAccess, 0, 1);
+    sem_init(&potIsNotFull, 0, 0);
+    sem_init(&potIsFull, 0, 0);
+    sem_init(&yourTurn, 0, 1);
     pthread_create(&bear, NULL, Bear, NULL);
-    for (i = 0; i < numberOfBees; i++)
-    {
+    for (i = 0; i < numberOfBees; i++) {
         pthread_create(&bee[i], NULL, Bee, (void*) i);
     }
-    for (i = 0; i < numberOfBees; i++)
-    {
+
+    pthread_join(bear, NULL);
+    for (i = 0; i < numberOfBees; i++) {
         pthread_join(bee[i], NULL);
     }
-    pthread_join(bear, NULL);
-
     sem_destroy(&potAccess);
     sem_destroy(&potIsFull);
-    sem_destroy(%potIsNotFull);
-
+    sem_destroy(&potIsNotFull);
+    sem_destroy(&yourTurn);
     return 0;
 }
